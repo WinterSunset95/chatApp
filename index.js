7995e2ae489ea6f2a79ddf9c92cc763d36466b6e
Winter Sunset <shirookamicod@gmail.com>
2021-11-19 12:19:34 -0500

    A new commit after a successful update

--- index.js
+++ index.js
@@ -5,25 +5,29 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-const session = require('session');
-app.use(session({secret: 'sshhh'}));
-
 app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/login.html');
+});
+
+app.get('/chat', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
+  socket.on('userConnect', (user) => {
+    console.log(user, ' connected');
+    io.emit('userConnect', (user));
   });
-  socket.on('chat message', (msg) => {
-    console.log(msg);
+  socket.on('chat message', (msg, name) => {
+    console.log(name, msg);
   });
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });   
+  socket.on('chat message', (msg, name) => {
+    io.emit('chat message', msg, name);
+  });
+     
 });
+
+app.use(express.static(__dirname));
 
 server.listen(3000, () => {
   console.log('Listening on port 3000');
