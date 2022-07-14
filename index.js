e089f1413cee84b2fb7d80322779ef473d920f4e
Winter <90015510+WinterSunset95@users.noreply.github.com>
2022-07-14 05:30:40 +0000

    Appending to log files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const fs = require('fs')
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
@@ -20,10 +21,21 @@ app.get('/logout', (req, res) => {
 io.on('connection', (socket) => {
   socket.on('userConnect', (user) => {
     console.log(user, ' connected');
+    fs.appendFile('./main/chat.log', user + ' connected \n', (err) => {
+      if (err) {
+        console.log(err)
+      }
+    })
     io.emit('userConnect', (user));
   });
   socket.on('chat message', (msg, name) => {
     console.log(name, msg);
+    const to_append = name + ' ' + msg + '\n'
+    fs.appendFile('./main/chat.log', to_append, (err) => {
+      if (err) {
+        console.log(err)
+      }
+    })
   });
   socket.on('chat message', (msg, name) => {
     io.emit('chat message', msg, name);
